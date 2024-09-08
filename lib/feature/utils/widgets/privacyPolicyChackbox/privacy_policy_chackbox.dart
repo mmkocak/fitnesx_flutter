744287a2318b37adfee874cd206e8cf9f064284e
mmkocak <muhammetlocak774@gmail.com>
2024-09-08 17:25:33 +0300

    policy chackbox bitimi

--- lib/feature/utils/widgets/privacyPolicyChackbox/privacy_policy_chackbox.dart
+++ lib/feature/utils/widgets/privacyPolicyChackbox/privacy_policy_chackbox.dart
@@ -6,19 +6,59 @@ class PrivacyPolicyChackbox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.sizeOf(context).width;
+    double screenHeight = MediaQuery.sizeOf(context).height;
     bool isChecked = false;
     return BlocProvider(
       create: (context) => PrivacyPolicyCubit(),
-      child: Padding (
-          padding: EdgeInsets.all(screenWidth * 0.01),
+      child: Padding(
+        padding: EdgeInsets.all(screenWidth * 0.01),
         child: Row(
           children: [
             BlocBuilder<PrivacyPolicyCubit, bool>(
-              builder: (context, state){
-                return Checkbox(value: state, onChanged: (bool? value){
-                  context.read<PrivacyPolicyCubit>().toggleCheckbox();
-                },);
+              builder: (context, state) {
+                return Checkbox(
+                  value: state,
+                  onChanged: (bool? value) {
+                    context.read<PrivacyPolicyCubit>().toggleCheckbox();
+                  },
+                );
               },
+            ),
+            Expanded(
+              child: RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "By continuing you accept our ",
+                      style: TextStyle(
+                          fontFamily: "Poppins", color: AppColors.gray_2, fontSize: screenWidth * 0.026),
+                    ),
+                    
+                    TextSpan(
+                      text: "Privacy Policy ",
+                      style: TextStyle(
+                          fontFamily: "Poppins",
+                          color: AppColors.gray_2,
+                          decoration: TextDecoration.underline,  fontSize: screenWidth * 0.026),
+                    ),
+                    TextSpan(
+                      text: "and ",
+                      style: TextStyle(
+                          fontFamily: "Poppins",
+                          color: AppColors.gray_2,
+                          fontSize: screenWidth * 0.026),
+                    ),
+                     TextSpan(
+                      text: "Term of use ",
+                      style: TextStyle(
+                          fontFamily: "Poppins",
+                          color: AppColors.gray_2,
+                          decoration: TextDecoration.underline,
+                          fontSize: screenWidth * 0.026),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
