d75664a2fa7ced75f22f1e9ce8f43033cc044171
mmkocak <muhammetlocak774@gmail.com>
2024-09-28 16:00:53 +0300

    gradle sürümü güncellendi

--- lib/core/firebase/auth/facebook_auth.dart
+++ lib/core/firebase/auth/facebook_auth.dart
@@ -1,12 +1,16 @@
+import 'dart:developer';
+
 import 'package:fitnesx_flutter/feature/utils/common/common_imports.dart';
 
 Future<UserCredential> signInWithFacebook() async {
   // Trigger the sign-in flow
   final LoginResult loginResult = await FacebookAuth.instance.login();
-
+  log(loginResult.accessToken!.tokenString.toString());
+  log(loginResult.message.toString());
   // Create a credential from the access token
   final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(loginResult.accessToken!.tokenString);
-
+    log(loginResult.accessToken!.tokenString.toString());
+  log(loginResult.message.toString());
   // Once signed in, return the UserCredential
   return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
 }
