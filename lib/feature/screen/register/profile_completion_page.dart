092268e3cbe98c4058fb5d2cbd8ebb18daaaa0f1
mmkocak <muhammetlocak774@gmail.com>
2024-10-04 17:23:53 +0300

    hata düzenlemesi

--- lib/feature/screen/register/profile_completion_page.dart
+++ lib/feature/screen/register/profile_completion_page.dart
@@ -29,191 +29,202 @@ class ProfileCompletionPage extends StatelessWidget {
     return BlocProvider(
       create: (context) => GenderBloc(),
       child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.symmetric(
-              horizontal: screenWidth * 0.01, vertical: screenHeight * 0.01),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(imageName),
-              SizedBox(
-                height: screenHeight * 0.04,
-              ),
-              Text(
-                "Let's complate your profile",
-                style: TextStyle(
-                    fontFamily: "Poppins",
-                    fontSize: screenWidth * 0.05,
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.blackColor),
-              ),
-              SizedBox(
-                height: screenHeight * 0.01,
-              ),
-              Text(
-                "it will help us to know more about you!",
-                style: TextStyle(
-                    color: AppColors.gray_1,
-                    fontFamily: "Poppins",
-                    fontSize: screenWidth * 0.03),
-              ),
-              SizedBox(
-                height: screenHeight * 0.03,
-              ),
-              Form(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
-                  child: Column(
-                    children: [
-                      BlocBuilder<GenderBloc, String?>(
-                        builder: (context, selectedGdender) {
-                          return Container(
-                            width: screenWidth * 0.9,
-                            child: DropdownButtonFormField(
-                              menuMaxHeight: screenHeight * 0.1,
-                              isExpanded: false,
-                              dropdownColor: AppColors.borderColor,
-                              decoration: InputDecoration(
-                                prefixIcon: const Icon(
-                                  Icons.supervised_user_circle,
-                                  color: AppColors.gray_1,
-                                ),
-                                hintText: "Choose Gender",
-                                hintStyle: TextStyle(
-                                  color: AppColors.gray_2,
-                                  fontFamily: "Poppins",
-                                  fontSize: screenWidth * 0.03,
-                                ),
-                                filled: true,
-                                fillColor: AppColors.borderColor,
-                                border: OutlineInputBorder(
-                                  borderSide: const BorderSide(
-                                      color: AppColors.borderColor),
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide: const BorderSide(
-                                      color: AppColors.borderColor),
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: const BorderSide(
-                                    color: AppColors.borderColor,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: screenWidth * 0.01, vertical: screenHeight * 0.01),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(imageName),
+                SizedBox(
+                  height: screenHeight * 0.02,
+                ),
+                Text(
+                  "Let's complate your profile",
+                  style: TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: screenWidth * 0.05,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.blackColor),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.01,
+                ),
+                Text(
+                  "it will help us to know more about you!",
+                  style: TextStyle(
+                      color: AppColors.gray_1,
+                      fontFamily: "Poppins",
+                      fontSize: screenWidth * 0.03),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.03,
+                ),
+                Form(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
+                    child: Column(
+                      children: [
+                        BlocBuilder<GenderBloc, String?>(
+                          builder: (context, selectedGdender) {
+                            return Container(
+                              width: screenWidth * 0.9,
+                              child: DropdownButtonFormField(
+                                menuMaxHeight: screenHeight * 0.1,
+                                isExpanded: false,
+                                dropdownColor: AppColors.borderColor,
+                                decoration: InputDecoration(
+                                  prefixIcon: const Icon(
+                                    Icons.supervised_user_circle,
+                                    color: AppColors.gray_1,
                                   ),
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                              ),
-                              value: context.watch<GenderBloc>().state,
-                              items: ['Erkek', 'Kadın']
-                                  .map((String gender) => DropdownMenuItem(
-                                        child: Text(
-                                          gender,
-                                          style: TextStyle(
-                                            fontFamily: "Poppins",
-                                            fontSize: screenWidth * 0.04,
-                                          ),
-                                        ),
-                                        value: gender,
-                                      ))
-                                  .toList(),
-                              onChanged: (String? newvalue) {
-                                context
-                                    .read<GenderBloc>()
-                                    .SelectGender(newvalue!);
-                              },
-                            ),
-                          );
-                        },
-                      ),
-                      SizedBox(
-                        height: screenHeight * 0.03,
-                      ),
-                      GestureDetector(
-                        onTap: () => _selectDate(context),
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                              vertical: screenHeight * 0.015,
-                              horizontal: screenWidth * 0.04),
-                          decoration: BoxDecoration(
-                            color: AppColors.borderColor,
-                            borderRadius: BorderRadius.circular(20),
-                            border: Border.all(color: AppColors.borderColor),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              const Icon(
-                                Icons.today_outlined,
-                                color: AppColors.gray_1,
-                              ),
-                              SizedBox(
-                                width: screenWidth * 0.03,
-                              ),
-                              Expanded(
-                                child: Text(
-                                  selectedDate == DateTime.now()
-                                      ? "Date Of Brith"
-                                      : "${selectedDate.toLocal()}"
-                                          .split(' ')[0],
-                                  style: TextStyle(
+                                  hintText: "Choose Gender",
+                                  hintStyle: TextStyle(
                                     color: AppColors.gray_2,
                                     fontFamily: "Poppins",
                                     fontSize: screenWidth * 0.03,
+                                  
+                                  ),
+                                  filled: true,
+                                  fillColor: AppColors.borderColor,
+                                  border: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color: AppColors.borderColor),
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color: AppColors.borderColor),
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                      color: AppColors.borderColor,
+                                    ),
+                                    borderRadius: BorderRadius.circular(20),
                                   ),
                                 ),
+                                value: context.watch<GenderBloc>().state,
+                                items: ['Erkek', 'Kadın']
+                                    .map((String gender) => DropdownMenuItem(
+                                          child: Text(
+                                            gender,
+                                            style: TextStyle(
+                                              fontFamily: "Poppins",
+                                              fontSize: screenWidth * 0.04,
+                                            ),
+                                          ),
+                                          value: gender,
+                                        ))
+                                    .toList(),
+                                onChanged: (String? newvalue) {
+                                  context
+                                      .read<GenderBloc>()
+                                      .SelectGender(newvalue!);
+                                },
                               ),
-                            ],
+                            );
+                          },
+                        ),
+                        SizedBox(
+                          height: screenHeight * 0.03,
+                        ),
+                        GestureDetector(
+                          onTap: () => _selectDate(context),
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: screenHeight * 0.015,
+                                horizontal: screenWidth * 0.04),
+                            decoration: BoxDecoration(
+                              color: AppColors.borderColor,
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(color: AppColors.borderColor),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                const Icon(
+                                  Icons.today_outlined,
+                                  color: AppColors.gray_1,
+                                ),
+                                SizedBox(
+                                  width: screenWidth * 0.03,
+                                ),
+                                Expanded(
+                                  child: Text(
+                                    selectedDate == DateTime.now()
+                                        ? "Date Of Brith"
+                                        : "${selectedDate.toLocal()}"
+                                            .split(' ')[0],
+                                    style: TextStyle(
+                                      color: AppColors.gray_2,
+                                      fontFamily: "Poppins",
+                                      fontSize: screenWidth * 0.03,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: screenHeight * 0.03,
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: screenWidth * 0.7,
-                            child: CustomComplateProfileTextField(  hinText: "Your Weight", screenWidth: screenWidth),
-                          ),
-                          SizedBox(
-                            width: screenWidth * 0.05,
-                          ),
-                          Expanded(
-                            child: CustomLinearPurpleContainer( text: "KG",  screenWidth: screenWidth, screenHeight: screenHeight),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: screenHeight * 0.03,
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: screenWidth * 0.7,
-                            child: CustomComplateProfileTextField(  hinText: "Your Height", screenWidth: screenWidth),
-                          ),
-                          SizedBox(
-                            width: screenWidth * 0.05,
-                          ),
-                          Expanded(
-                            child: CustomLinearPurpleContainer( text: "CM", screenWidth: screenWidth, screenHeight: screenHeight),
-                          ),
-                        ],
-                      ),
-                    ],
+                        SizedBox(
+                          height: screenHeight * 0.03,
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: screenWidth * 0.7,
+                              child: CustomComplateProfileTextField(
+                                  hinText: "Your Weight",
+                                  screenWidth: screenWidth),
+                            ),
+                            SizedBox(
+                              width: screenWidth * 0.05,
+                            ),
+                            Expanded(
+                              child: CustomLinearPurpleContainer(
+                                  text: "KG",
+                                  screenWidth: screenWidth,
+                                  screenHeight: screenHeight),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: screenHeight * 0.03,
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: screenWidth * 0.7,
+                              child: CustomComplateProfileTextField(
+                                  hinText: "Your Height",
+                                  screenWidth: screenWidth),
+                            ),
+                            SizedBox(
+                              width: screenWidth * 0.05,
+                            ),
+                            Expanded(
+                              child: CustomLinearPurpleContainer(
+                                  text: "CM",
+                                  screenWidth: screenWidth,
+                                  screenHeight: screenHeight),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: screenHeight * 0.03,),
+                        CustomElevetadButton(onPressed: (){}, text: "Next", height: screenHeight * 0.07, width: screenWidth * 0.9)
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-
-
-
